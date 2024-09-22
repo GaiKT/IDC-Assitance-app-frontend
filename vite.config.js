89e91685b87d4fbd78a4fe918b91900529cafe92
GaiKT <124340777+GaiKT@users.noreply.github.com>
2024-09-22 19:06:10 +0700

    feat : install dotenv and confid it

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+import { resolve } from 'path';
+
+// Load .env file from the parent directory
+dotenv.config({ path: resolve(__dirname, '../.env') });
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 100000000, // Set a higher chunk size warning limit (in bytes)
+  },
+});
