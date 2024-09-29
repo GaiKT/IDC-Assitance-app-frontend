a6eb8884cf08559ed186650a85831204a3b9b0c0
GaiKT <124340777+GaiKT@users.noreply.github.com>
2024-09-29 18:28:19 +0700

    feat: validation user password with Yup-password

--- src/pages/auth/ResetPassPage.jsx
+++ src/pages/auth/ResetPassPage.jsx
@@ -6,15 +6,21 @@ import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup"
-import { useNavigate } from "react-router-dom";
+import YupPassword from 'yup-password';
+YupPassword(yup);
 
 function ResetPage() {
   const [hide , setHide] = useState(true)
-  const navigate = useNavigate()
 
   const schema = yup.object({
     old_password: yup.string().required("Old Password is required"),
-    password: yup.string().required("Password is required"),
+    password: yup.string().required("Password is required")
+      .min(
+        8,'password must contain 8 or more characters with at least one of each: uppercase, lowercase, number and special')
+      .minLowercase(1, 'password must contain at least 1 lower case letter')
+      .minUppercase(1, 'password must contain at least 1 upper case letter')
+      .minNumbers(1, 'password must contain at least 1 number')
+      .minSymbols(1, 'password must contain at least 1 special character'),
     confirmPassword: yup.string()
       .oneOf([yup.ref('password'), null], 'Passwords must match')
       .required('Confirm Password is required'),
